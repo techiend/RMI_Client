0ef31a825e46639e85dce4429ebf935ab42c7300
Carlos Verde <cverde@domain.com>
2021-08-01 20:24:19 -0400

    Retiro de cuenta.

--- src/main/java/com/distribuidos/model/Deposit.java
+++ src/main/java/com/distribuidos/model/Deposit.java
@@ -3,7 +3,10 @@ package com.distribuidos.model;
 import java.util.Date;
 
 public class Deposit extends Transaction{
-    public Deposit(float amount, Date date, String desc, int sourceNumber, int destinationNumber) {
-        super( amount, date, desc, sourceNumber, destinationNumber, "Deposit");
+    public Deposit() {
+    }
+
+    public Deposit(float amount, String desc, int sourceNumber, int destinationNumber) {
+        super(amount, desc, sourceNumber, destinationNumber, "Deposit");
     }
 }
--- src/main/java/com/distribuidos/model/Withdrawal.java
+++ src/main/java/com/distribuidos/model/Withdrawal.java
@@ -3,7 +3,10 @@ package com.distribuidos.model;
 import java.util.Date;
 
 public class Withdrawal extends Transaction{
-    public Withdrawal(float amount, Date date, String desc, int sourceNumber, int destinationNumber) {
-        super(amount, date, desc, sourceNumber, destinationNumber, "Withdrawal");
+    public Withdrawal() {
+    }
+
+    public Withdrawal(float amount, String desc, int sourceNumber, int destinationNumber) {
+        super(amount, desc, sourceNumber, destinationNumber, "Withdrawal");
     }
 }
